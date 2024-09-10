c718725c6aa0a1687e9f5c452a4d5db707881bd7
nkdweb <nkdweb0@gmail.com>
2024-09-10 16:37:02 +0530

    Updated with input password

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 // run `node index.js` in the terminal
+console.log("Updating html file with tlemon component ...");
 
-//console.log(`Hello Node.js v${process.versions.node}!`);
-console.log("Updating html file with tlemon component ...")
-
-var fs = require('fs');
+var fs = require("fs");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
 function readFiles(dirname, onFileContent, onError) {
-  fs.readdir(dirname, function(err, filenames) {
+  fs.readdir(dirname, function (err, filenames) {
     if (err) {
       onError(err);
       return;
     }
-    filenames.forEach(function(filename) {
-      fs.readFile(dirname + filename, 'utf-8', function(err, content) {
+    filenames.forEach(function (filename) {
+      fs.readFile(dirname + filename, "utf-8", function (err, content) {
         if (err) {
           onError(err);
           return;
@@ -27,43 +25,36 @@ function readFiles(dirname, onFileContent, onError) {
 
 var data = {};
 readFiles(
-    "html/",
-    function (filename, content) {
-      let contentChanged = false;
-      let newContent = data[filename] = content;
-      const inputTags = content.match(/<input type="text" .*>/g) || [];
-      for (const tag of inputTags) {
-        const dom = new JSDOM(tag);
-        const input = dom.window.document.querySelector("input");
-        if (input.getAttribute("type") === "text") {
-          const attributeNames = input.getAttributeNames();
-          const attributeItems = attributeNames.reduce((acc, name) => {
-            const value = input.getAttribute(name);
-            if (value !== '') {
-              acc.push(`${name}="${value}"`);
-            } else {
-                acc.push(`${name}`)
-            }
-            return acc;
-          }, []);
-          const newTag = `<t-input ${attributeItems.join(" ")}></t-input>`;
-          if (newTag !== tag) {
-            newContent = newContent.replace(tag, newTag);
-            contentChanged = true;
-          }
-        }
-      }
-      if (contentChanged) {
-        fs.writeFile("html/" + filename, newContent, function (err) {
-          if (err) throw err;
-          console.log(`File "${filename}" updated.`);
-        });
-      } else {
-        console.log(`File "${filename}" unchanged.`);
-      }
-    },
-    function (err) {
-      console.log(err);
-      throw err;
-    }
-  );
+  "html/",
+  function (filename, content) {
+    const inputTags = content.match(/<input .*>/g) || [];
+    const newContent = inputTags.reduce((content, tag) => {
+      const dom = new JSDOM(tag);
+      const input = dom.window.document.querySelector("input");
+      const attributeNames = input.getAttributeNames();
+      const attributeItems = attributeNames.map((name) => {
+        const value = input.getAttribute(name);
+        return value !== "" ? `${name}="${value}"` : name;
+      });
+      const newTag =
+        input.getAttribute("type") === "text"
+          ? `<t-input ${attributeItems.join(" ")}></t-input>`
+          : `<t-input-password ${attributeItems.join(" ")}></t-input-password>`;
+      return content.replace(tag, newTag);
+    }, content);
+
+    return fs.promises
+      .writeFile("html/" + filename, newContent)
+      .then(() => {
+        console.log(`File "${filename}" updated.`);
+      })
+      .catch((err) => {
+        console.log(err);
+        throw err;
+      });
+  },
+  function (err) {
+    console.log(err);
+    throw err;
+  }
+);
